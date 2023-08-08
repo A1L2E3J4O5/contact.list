f45fd71a17f6d26d15870a40d8c5398a4275c59d
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-08 16:10:20 -0500

    tarea casi a completar modificar para mejor funcionamiento

--- entregable.js
+++ entregable.js
@@ -1,33 +1,56 @@
-let r = 
-[ 
-    "Jesus Ramirez",
-    "Alejandro Ruiz",
+let nombres = 
+[
+    "jesus ramirez",
+    "daniela dorado",
+    "alejandro ruiz",
 ]
 
-console.log
-(
-    r[0],
-)
-console.log
-(
-    r[1],
-)
 
+let dos = parseInt(prompt("elige \n 1 para agregar \n 2 para eliminar"));
 
-
-let u = prompt("Nombre y apellido");
-
-function agregarUsuario ( )
+if (dos == 1)
+{
+    for (u = 1; u <= 10; u++)
+     {
+        let usuario = u;
+        usuario = prompt("Nombre y Apellido");
+        nombres.push(usuario)
+        
+        if (usuario === null) {
+            console.log("Operación cancelada por el usuario.");
+            console.log("fin del registro");
+            break;
+        }   
+        console.log(nombres);
+    }   
+}
+else if(dos == 2)
 {
 
-    
-        let usuario = u
-        console.log(usuario);
-        u ++;
+    let borrar = parseInt(prompt("elige numero segun registro",));
+    prompt(nombres)
+    if(borrar == 0)
+    {
 
+        console.log(nombres[1]);
+        console.log(nombres[2]);
+        console.log("elemento: ",nombres[0], "eliminado")
+    }
+    if(borrar == 1)
+    {
+        console.log(nombres[0]);
+        console.log(nombres[2]);
+        console.log("elemento: ",nombres[1], "eliminado")
+    }
+    if(borrar == 2)
+    {
+        console.log(nombres[0]);
+        console.log(nombres[1]);
+        console.log("elemento: ",nombres[2], "eliminado")
+    }
+    if (borrar === null) {
+        console.log("Operación cancelada por el usuario.");
+        console.log("fin del registro");
+    } 
     
-
 }
-agregarUsuario()
-
-
