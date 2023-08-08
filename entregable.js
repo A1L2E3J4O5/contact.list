040a169579a2625fb7aee860a99a492c61ffad64
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-08-07 20:49:45 -0500

    primer entregable COMPLETAR

---
+++ entregable.js
@@ -0,0 +1,33 @@
+let r = 
+[ 
+    "Jesus Ramirez",
+    "Alejandro Ruiz",
+]
+
+console.log
+(
+    r[0],
+)
+console.log
+(
+    r[1],
+)
+
+
+
+let u = prompt("Nombre y apellido");
+
+function agregarUsuario ( )
+{
+
+    
+        let usuario = u
+        console.log(usuario);
+        u ++;
+
+    
+
+}
+agregarUsuario()
+
+
